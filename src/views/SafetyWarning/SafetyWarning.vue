555dc4cfaa6fc0833ac443a88c84f79316bbc1f3
栾天 <tian.luan@percent.cn>
2021-09-30 00:56:37 +0800

    a

--- src/views/SafetyWarning/SafetyWarning.vue
+++ src/views/SafetyWarning/SafetyWarning.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="safety-warning">
+    <common-header></common-header>
     安全预警
   </div>
 </template>
 
 <script>
+  import CommonHeader from "@/components/CommonHeader";
   export default {
     name: 'SafetyWarning',
+    components: { CommonHeader },
     data() {
       return {
 
@@ -17,6 +20,8 @@
 
 <style lang="scss" scoped>
   .safety-warning {
-
+    height: 100%;
+    background: url("~@/assets/img/index_bg.png") no-repeat;
+    background-size: 100% 100%;
   }
 </style>
