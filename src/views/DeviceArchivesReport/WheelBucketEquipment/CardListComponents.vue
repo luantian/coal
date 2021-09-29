e69fc20da93f9d1dd08491f5988e4d7cba71b287
Tencent <liyang19023@126.com>
2021-09-30 01:10:52 +0800

    aaaa

--- src/views/DeviceArchivesReport/WheelBucketEquipment/CardListComponents.vue
+++ src/views/DeviceArchivesReport/WheelBucketEquipment/CardListComponents.vue
@@ -4,12 +4,19 @@
       <el-row>
         <el-col :span="4" v-for="(item) in tableData" :key="item.id" :offset="1" >
           <div style="margin-top:15px">
-            <el-card :body-style="{ padding: '20px'}" shadow="hover" style="display: flex" >
-              <div style="flex: 1">
-                <span>{{item.deviceParam}} {{item.deviceNumberInfo}} {{item.deviceUnit}} </span><br>
+            <el-card :body-style="{ padding: '20px'}" shadow="hover" >
+              <div style="display: flex">
+                <div style="flex: 1">
+                  <span>{{item.deviceParam}} </span><br>
+                  <div style="padding-top: 5px">
+                    <span >{{item.deviceNumberInfo}} ({{item.deviceUnit}})</span>
+                  </div>
+                </div>
+                <div style="vertical-align: middle">
+                  <i class="el-icon-edit" style="vertical-align: middle"/>
+                </div>
               </div>
-              <div class="el-icon-edit">
-              </div>
+
             </el-card>
           </div>
         </el-col>
@@ -36,52 +43,56 @@
   import ArchiveReportModel from "@/models/ArchiveReport";
     export default {
         name: "addPersonnelFileForm",
+      props: {
+          // tableData: {}
+      },
       data() {
         return {
-          tableData: [
-            {
-              id: 1,
-              deviceParam: '11',
-              deviceNumberInfo: '22',
-              deviceUnit: '33'
-            },
-            {
-              id: 2,
-              deviceParam: '44',
-              deviceNumberInfo: '55',
-              deviceUnit: '66'
-            },
-            {
-              id: 3,
-              deviceParam: '77',
-              deviceNumberInfo: '88',
-              deviceUnit: '99'
-            },
-            {
-              id: 4,
-              deviceParam: 'aa',
-              deviceNumberInfo: 'bb',
-              deviceUnit: 'cc'
-            },
-            {
-              id: 5,
-              deviceParam: 'qq',
-              deviceNumberInfo: 'ww',
-              deviceUnit: 'ee'
-            },
-            {
-              id: 6,
-              deviceParam: 'ww',
-              deviceNumberInfo: 'ss',
-              deviceUnit: 'xx'
-            },
-            {
-              id: 7,
-              deviceParam: 'rr',
-              deviceNumberInfo: 'ff',
-              deviceUnit: 'vv'
-            }
-          ],
+          tableData: [],
+          // tableData: [
+          //   {
+          //     id: 1,
+          //     deviceParam: '11',
+          //     deviceNumberInfo: '22',
+          //     deviceUnit: '33'
+          //   },
+          //   {
+          //     id: 2,
+          //     deviceParam: '44',
+          //     deviceNumberInfo: '55',
+          //     deviceUnit: '66'
+          //   },
+          //   {
+          //     id: 3,
+          //     deviceParam: '77',
+          //     deviceNumberInfo: '88',
+          //     deviceUnit: '99'
+          //   },
+          //   {
+          //     id: 4,
+          //     deviceParam: 'aa',
+          //     deviceNumberInfo: 'bb',
+          //     deviceUnit: 'cc'
+          //   },
+          //   {
+          //     id: 5,
+          //     deviceParam: 'qq',
+          //     deviceNumberInfo: 'ww',
+          //     deviceUnit: 'ee'
+          //   },
+          //   {
+          //     id: 6,
+          //     deviceParam: 'ww',
+          //     deviceNumberInfo: 'ss',
+          //     deviceUnit: 'xx'
+          //   },
+          //   {
+          //     id: 7,
+          //     deviceParam: 'rr',
+          //     deviceNumberInfo: 'ff',
+          //     deviceUnit: 'vv'
+          //   }
+          // ],
           rules: {
             deviceParam: [
               { required: true, message: '设备参数不能为空', trigger: 'blur' }
@@ -97,29 +108,16 @@
         };
       },
       methods: {
-        async submitForm(formName) {
-          this.$refs[formName].validate(async (valid) => {
-            if (valid) {
-              const {code} = await ArchiveReportModel.addWheelBucketBasicFile(this.ruleForm)
-              if(code === 200){
-                this.$message({
-                  message: '新增成功',
-                  type: 'success'
-                });
-                this.$emit("addDialogClose")
-                this.$emit("queryList")
-                return
-              }
-              this.$message.error('新增失败');
-            } else {
-              console.log('error submit!!');
-              return false;
-            }
-          });
+        async toQuery() {
+          const {rows} = await ArchiveReportModel.selectWheelBucketBasicFilesList()
+          this.tableData = rows
         },
         addCancel() {
           this.$emit("addDialogClose")
         }
+      },
+      created() {
+        this.toQuery()
       }
     }
 </script>
