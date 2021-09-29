19a9c3ea3718483f39530716dafa6443a2a42d84
ly <liyang199023@126.com>
2021-09-29 20:51:30 +0800

    aa

--- src/models/ArchiveReport.js
+++ src/models/ArchiveReport.js
@@ -28,10 +28,27 @@ class ArchiveReport extends Base {
     })
   }
 
+
   // 編輯现场人员基础档案
   static async editSitePersonnelFile(data) {
     return await this.put({
       url: '/person/info',
+      data
+    })
+  }
+
+  // 查询轮斗挖掘机故障档案列表
+  static async selectWheelBucketBreakDown(data) {
+    return await this.get({
+      url: '/faualwheel/faualwheelInfo/list',
+      data
+    })
+  }
+
+  // 新增轮斗挖掘机故障档案列表
+  static async addWheelBucketBreakDown(data) {
+    return await this.post({
+      url: '/faualwheel/faualwheelInfo',
       data
     })
   }
